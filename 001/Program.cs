2fe7e232c64a32c51192dc7f2a4388771ed31f55
Anadell <lemeshkoav@yandex.ru>
2023-03-02 21:46:22 +0600

    3 попытка загрузки  папки 001 с gitignore

---
+++ 001/Program.cs
@@ -0,0 +1,10 @@
+﻿// 1. С клавиатуры вводится целое число. Вывести квадрат числа
+
+int num;
+Console.Write("Enter an integer: ");
+string a = Console.ReadLine();
+num = Convert.ToInt32(a);
+System.Console.Write("The square of the number '");
+Console.Write(a);
+Console.Write("' = ");
+Console.Write(Math.Pow(num,2));
