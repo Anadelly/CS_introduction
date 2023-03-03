a78cca0ec85ad09997162b591bba12908a7ae05d
Anadell <lemeshkoav@yandex.ru>
2023-03-03 17:55:27 +0600

    Lesson 1 over

---
+++ 009/Program.cs
@@ -0,0 +1,14 @@
+﻿// 9. Вывести на экран чётные числа от 1 до N
+
+int i=2;
+int N;
+N=Convert.ToInt32(Console.ReadLine());
+// нц пока i<=N
+
+while(i<=N)
+{
+    System.Console.WriteLine(i);
+    i=i+2;
+}
+
+//кц
