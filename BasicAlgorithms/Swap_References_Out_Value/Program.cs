600debfbf4eb2f618f90c0a79e586e867c2eb9eb
Anadell <lemeshkoav@yandex.ru>
2023-03-31 20:35:07 +0300

    Семинар 3 ч.1

---
+++ BasicAlgorithms/Swap_References_Out_Value/Program.cs
@@ -0,0 +1,35 @@
+﻿
+// void Swap(int x, int y) //передача по значению (Value)
+// {
+//    int t = x;
+//        x = y;
+//        y = t; 
+// }
+
+void Swap(ref int x, ref int y) //передача по ссылке (References)
+{
+   int t = x;
+       x = y;
+       y = t; 
+}
+
+void Calculator (int a, int b, out int sum, out int sub) //передача по ссылке без присвоения первоначальных значений
+{
+    sum = a + b;
+    sub = a - b;
+}
+
+int a = 4;
+int b = 5;
+int s, sb;
+Calculator(a, b, out s, out sb);
+
+System.Console.WriteLine($"{a} + {b} = {s}");
+System.Console.WriteLine($"{a} - {b} = {sb}");
+
+// System.Console.WriteLine($"a={a} b={b}");
+// Swap(ref a, ref b);
+// // a = a + b;
+// // b = a - b;
+// // a = a - b;
+// System.Console.WriteLine($"a={a} b={b}");
